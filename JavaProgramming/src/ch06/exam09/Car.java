c35e85630e4328c60d1d148597d968872ce38f27
Administrator <Administrator@COM-PC>
2016-08-24 18:13:26 +0900

    클래스

---
+++ JavaProgramming/src/ch06/exam09/Car.java
@@ -0,0 +1,57 @@
+package ch06.exam09;
+
+public class Car {
+
+	//Field
+	int gas;
+	
+	//Constructor
+	
+	
+	//Method
+	void setGas(int gas){
+		this.gas = gas;		
+	}
+	
+	boolean isLeftGas(){
+		if (gas == 0){
+			System.out.println("gas가 없습니다.");
+			return false;
+		} else {
+			System.out.println("gas가 있습니다.");
+			return true;
+		}
+	}
+	
+	void run(){
+		while(true){
+			if(gas > 0){
+				System.out.println("달립니다.(gas 잔량 : " + gas + ")");
+				gas -= 1;
+				
+			} else {
+				System.out.println("멈춥니다.(gas 잔량 : " + gas + ")");
+				//break;	// break 는 while 문 밖으로 나가서 계속 실행
+				return;	// return 하지 않지만 여기서 끝내겠다. 
+							// while 문 밖으로 나가지 않고 메소드 실행 종료. 
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ JavaProgramming/src/ch06/exam10/Calculator.java
@@ -0,0 +1,19 @@
+package ch06.exam10;
+
+public class Calculator {
+	//Field
+	
+	//Constuctor
+	
+	//Method
+	
+	//정사각형의 넓이
+	double areaRectangle(double width){
+		return width * width;
+	}	
+	
+	//직사각형의 넓이
+	double areaRectangle(double width, double height){
+		return width * height;
+	}
+}
