81f5dae3e3988d4780305974a52769e039833c38
cisnice <cisnice@naver.com>
2016-09-29 11:02:32 +0900

    MySpring

--- MySpring/src/main/java/com/mycompany/myspring/HomeController.java
+++ MySpring/src/main/java/com/mycompany/myspring/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping("/")
 	public String home() {
 		System.out.println("println()");
 		logger.info("logger.info 출력");		
---
+++ MySpring/src/main/java/com/mycompany/myspring/MemberController.java
@@ -0,0 +1,37 @@
+package com.mycompany.myspring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class MemberController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+	@RequestMapping("/member/join")
+	public String join() {		
+		logger.info("join");		
+		return "member/join";
+	}
+	
+	@RequestMapping("/member/login")
+	public String login() {		
+		logger.info("login");		
+		return "member/login";
+	}
+	
+	@RequestMapping("/member/logout")
+	public String logout() {		
+		logger.info("logout");		
+		return "member/logout";
+	}
+	
+	@RequestMapping("/member/withdraw")
+	public String withdraw() {		
+		logger.info("withdraw");		
+		return "member/withdraw";
+	}
+	
+}
